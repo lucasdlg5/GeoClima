49524a276e67304154e1cc42fe0a3dfb7d8d788e
Lucas Domingos Leão Gomes <lucasdomingosleao55@hotmail.com>
2022-10-14 00:28:29 -0300

    Correcao Bug da funcao buscaPorNome

--- GeoLocalizaConsole/GeoLocalizaConsole/Program.cs
+++ GeoLocalizaConsole/GeoLocalizaConsole/Program.cs
@@ -1,11 +1,18 @@
 ﻿Console.WriteLine("Hello, World!");
 
-
-
 // Latitude 23 sul e 45 Oeste -- São Jose dos Campos - São Paulo
 // 35 Norte e 139 Leste - Japão - Tokyo
 // 57 Norte e 65 Leste - Tyumen - Sibéria - Russia
 
+Dictionary<string, string> dados = new Dictionary<string, string>();
+dados.Add("Sao Jose dos Campos", "23 sul e 45 Oeste");
+dados.Add("Tokyo", "35 Norte e 139 Leste");
+dados.Add("Tyumen", "57 Norte e 65 Leste");
+
+
+
+//string[] list = { "Sao Jose dos Campos", "Tokyo", "Tyumen" };
+//string[] coordenadas = { "23 sul e 45 Oeste" , "35 Norte e 139 Leste", "57 Norte e 65 Leste" };
 
 string buscaCidadeLatitudeLongitude(string latitude, string longitude)
 {
@@ -13,30 +20,25 @@ string buscaCidadeLatitudeLongitude(string latitude, string longitude)
     return "";
 }
 
-Dictionary<string, string> dados = new Dictionary<string, string>();
-dados.Add("Sao Jose dos Campos", "23 sul e 45 Oeste");
-dados.Add("Tokyo", "35 Norte e 139 Leste");
-dados.Add("Tyumen", "57 Norte e 65 Leste");
-
 string buscaPorNome(string cidade)
 {
+    //for (int i = 0; i < cidade.Count(); i++)
+    //{
+    //    if (list[i].Equals(cidade))
+    //    {
+    //        return $"{list[i]} - {coordenadas[i]}";
+    //    }
+    //}
+
     foreach (var item in dados)
     {
-       if (item.Key.Equals("Sao Jose dos Campos")){
-            return "Sao Jose dos Campos";
-       }
-       if (item.Key.Equals("Tokyo")){
-            return "Tokyo";
-       }
-       if (item.Key.Equals("Tyumen"))
-       {
-            return "Tyumen";
-       }
+        if (item.Key.Equals(cidade))
+        {
+            return $"{item.Key} - {item.Value}";
+        }
     }
-    return "Nao ha cidade disponivel";
+    return "N/A";
 }
-
-
 
 Console.WriteLine(buscaPorNome("Sao Jose dos Campos")); // Sao jose dos campos
 Console.WriteLine(buscaPorNome("Sao")); // Nao ha cidade disponivel
